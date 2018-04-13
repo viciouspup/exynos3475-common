6d687db4ef76d0c384df3c30e2803c310116f72d
lzzy12 <jhashivam2020@gmail.com>
2018-04-13 14:07:15 +0530

    Some improvements in build scripts
    Signed-off-by: lzzy12 <jhashivam2020@gmail.com>

--- build.sh
+++ build.sh
@@ -4,12 +4,11 @@ RDIR=$(pwd)
 # GCC
 export CROSS_COMPILE=../linaro-7/bin/arm-eabi-
 # J200G MM Defcon
-make j2lteswa_N_defconfig -j8
-make exynos3475-j2lte_swa_open_00.dtb exynos3475-j2lte_swa_open_01.dtb exynos3475-j2lte_swa_open_02.dtb exynos3475-j2lte_swa_open_03.dtb exynos3475-j2lte_swa_open_04.dtb -j8
+make j2lteswa_N_defconfig -j$(nproc --all)
+make exynos3475-j2lte_swa_open_00.dtb exynos3475-j2lte_swa_open_01.dtb exynos3475-j2lte_swa_open_02.dtb exynos3475-j2lte_swa_open_03.dtb exynos3475-j2lte_swa_open_04.dtb -j$(nproc --all)
 # Make zImage
-make ARCH=arm -j8
-# Make DT.img
-./tools/dtbtool -o ./boot.img-dtb -v -s 2048 -p ./scripts/dtc/ $DTS/
+make ARCH=arm $(nproc --all)
+
 # Cleaup
 rm -rf $DTS/.*.tmp
 rm -rf $DTS/.*.cmd
